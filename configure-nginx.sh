05cfcf7d1856a4a23aaf6d013b1ab6bc418bbcb4
dursm9995 <81348212+dursm9995@users.noreply.github.com>
2024-12-24 14:08:53 +0900

    Update configure-nginx.sh

--- configure-nginx.sh
+++ configure-nginx.sh
@@ -7,4 +7,4 @@ sudo apt-get update
 sudo apt-get install -y nginx
 
 # Set the home page.
-echo "<html><body><h2>Num 2024123, Name Hong Gil-dong</h2></body></html>" | sudo tee -a /var/www/html/index.html
+echo "<html><body><h2>Num 2024123, Name Hong Gildong</h2></body></html>" | sudo tee -a /var/www/html/index.html
